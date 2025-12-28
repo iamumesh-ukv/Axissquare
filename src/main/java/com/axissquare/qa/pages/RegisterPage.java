2eacd79b924423a484c02954199f5b31195d3660
umesh.ukv131992@gmail.com <umesh.ukv131992@gmail.com>
2025-12-28 16:38:44 +0530

    28-12-2025 Updated Register page And RegisterTest

--- src/main/java/com/axissquare/qa/pages/RegisterPage.java
+++ src/main/java/com/axissquare/qa/pages/RegisterPage.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import java.time.Duration;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegisterPage {
 	WebDriver driver;
@@ -23,14 +26,33 @@ public class RegisterPage {
 	WebElement enterPhoneNumber;
 	@FindBy(xpath = "//input[@name='register_pass']")
 	WebElement enterPassword;
-	@FindBy(xpath = "//input[@placeholder='Retype Password']")
+	@FindBy(xpath = "//input[@name='register_pass_retype']")
 	WebElement retypePassword;
-	@FindBy(xpath = "//ul[@role='presentation']//li")
+	@FindBy(xpath = "//div[contains(text(),'Select your account type')]")
+	WebElement selectAccountTypeDropdown;
+	@FindBy(xpath = "//ul[@class='dropdown-menu inner show']//li")
 	List<WebElement> selectAccountList;
 	@FindBy(xpath = "//label[@class='control control--checkbox']")
 	WebElement checkTermsAndConditions;
 	@FindBy(xpath = "//button[@id='houzez-register-btn']")
 	WebElement clickOnRegisterButton;
+	// validation locators
+	@FindBy(xpath = "//div[@role='alert']")
+	WebElement firstNameWarning;
+	@FindBy(xpath = "//div[contains(text(),'The last name field is empty.')]")
+	WebElement lastNameWarning;
+	@FindBy(xpath = "//div[contains(text(), 'The username field is empty.')]")
+	WebElement userNameWarning;
+	@FindBy(xpath = "//div[contains(text(),'The email field is empty.')]")
+	WebElement emailWarning;
+	@FindBy(xpath = "//div[contains(text(), 'Please enter your number')]")
+	WebElement phoneNumberWarning;
+	@FindBy(xpath = "//div[contains(text(), 'One of the password field is empty!')]")
+	WebElement passwordWarning;
+	@FindBy(xpath = "//div[contains(text(), 'Passwords do not match')]")
+	WebElement passwordNotMatchingWarning;
+	@FindBy(xpath = "//div[contains(text(), 'You need to agree with terms & conditions.')]")
+	WebElement termsAndConditionsWarning;
 
 	public RegisterPage(WebDriver driver) {
 		this.driver = driver;
@@ -38,50 +60,100 @@ public class RegisterPage {
 	}
 
 	// Actions
-/*	public void enterFirstName(String firstNameText) {
-		enterFirstName.sendKeys(firstNameText);
+	/*
+	 * public void enterFirstName(String firstNameText) {
+	 * enterFirstName.sendKeys(firstNameText); }
+	 * 
+	 * public void enterLastName(String lastNameText) {
+	 * enterLastName.sendKeys(lastNameText); }
+	 * 
+	 * public void enterEmailAddress(String emailAddressText) {
+	 * enterEmailAddress.sendKeys(emailAddressText); }
+	 * 
+	 * public void enterPhoneNumber(String phoneNumber) {
+	 * enterPhoneNumber.sendKeys(phoneNumber); }
+	 * 
+	 * public void enterPassword(String passwordText) {
+	 * enterPassword.sendKeys(passwordText); }
+	 * 
+	 * public void reEnterPassword(String retypePasswordText) {
+	 * retypePassword.sendKeys(retypePasswordText); }
+	 * 
+	 * public void selectAccountType(WebElement accountType) { for (WebElement
+	 * account : selectAccountList) { if
+	 * (account.getText().equals(selectAccountList)) { account.click(); } } }
+	 * 
+	 * public RegisterPage selectTermsAndConditions() {
+	 * checkTermsAndConditions.click(); return new RegisterPage(driver); }
+	 * 
+	 * public RegisterPage ClickOnRegisterButton() { clickOnRegisterButton.click();
+	 * return new RegisterPage(driver); }
+	 */
+
+	// Generic reusable method (ONLY ONCE)
+	public String getWarningText(WebElement element) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+			wait.until(ExpectedConditions.visibilityOf(element));
+			return element.isDisplayed() ? element.getText().trim() : "";
+		} catch (Exception e) {
+			System.out.println("Warning not displayed: " + element);
+			return "";
+		}
 	}
 
-	public void enterLastName(String lastNameText) {
-		enterLastName.sendKeys(lastNameText);
+	// First Name warning
+	public String retrieveFirstNameWarning() {
+		return getWarningText(firstNameWarning);
 	}
 
-	public void enterEmailAddress(String emailAddressText) {
-		enterEmailAddress.sendKeys(emailAddressText);
+	// Last Name warning
+	public String retrieveLastNameWarning() {
+		return getWarningText(lastNameWarning);
 	}
 
-	public void enterPhoneNumber(String phoneNumber) {
-		enterPhoneNumber.sendKeys(phoneNumber);
+	// User name warning
+	public String retrieveUserNameWarning() {
+		return getWarningText(userNameWarning);
 	}
 
-	public void enterPassword(String passwordText) {
-		enterPassword.sendKeys(passwordText);
+	// Email warning
+	public String retrieveEmailWarning() {
+		return getWarningText(emailWarning);
 	}
 
-	public void reEnterPassword(String retypePasswordText) {
-		retypePassword.sendKeys(retypePasswordText);
+	// Phone number warning
+	public String retrievePhoneNumberWarning() {
+		return getWarningText(phoneNumberWarning);
 	}
 
-	public void selectAccountType(WebElement accountType) {
+	// Password empty warning
+	public String retrievePasswordWarning() {
+		return getWarningText(passwordWarning);
+	}
+
+	// Password mismatch warning
+	public String retrievePasswordNotMatchingWarning() {
+		return getWarningText(passwordNotMatchingWarning);
+	}
+
+	// Terms & Conditions warning
+	public String retrieveTermsAndConditionsWarning() {
+		return getWarningText(termsAndConditionsWarning);
+	}
+
+	public void selectAccountType(String accountList) {
 		for (WebElement account : selectAccountList) {
-			if (account.getText().equals(selectAccountList)) {
+			if (account.getText().equalsIgnoreCase(accountList)) {
 				account.click();
+				break;
 			}
 		}
 	}
 
-	public RegisterPage selectTermsAndConditions() {
-		checkTermsAndConditions.click();
-		return new RegisterPage(driver);
-	}
-
-	public RegisterPage ClickOnRegisterButton() {
-		clickOnRegisterButton.click();
-		return new RegisterPage(driver);
-	}
-*/
-	public RegisterPage registerWithMandatoryFields(String firstNameText, String lastNameText,String userNameText, String emailText,String userPhoneText,
-			String userPasswordText, String userReTypePassword ) {
+	public RegisterPage registerWithMandatoryFields(String firstNameText, String lastNameText, String userNameText,
+			String emailText, String userPhoneText, String userPasswordText, String userReTypePasswordText,
+			String accountTypeText) {
 
 		enterFirstName.sendKeys(firstNameText);
 		enterLastName.sendKeys(lastNameText);
@@ -89,7 +161,10 @@ public class RegisterPage {
 		enterEmailAddress.sendKeys(emailText);
 		enterPhoneNumber.sendKeys(userPhoneText);
 		enterPassword.sendKeys(userPasswordText);
-		retypePassword.sendKeys(userReTypePassword);
+		retypePassword.sendKeys(userReTypePasswordText);
+		selectAccountTypeDropdown.click();
+		selectAccountType(accountTypeText);
+		checkTermsAndConditions.click();
 		clickOnRegisterButton.click();
 
 		return new RegisterPage(driver);
--- src/test/java/com/axissquare/qa/testcases/RegisterTest.java
+++ src/test/java/com/axissquare/qa/testcases/RegisterTest.java
@@ -1,6 +1,8 @@
 package com.axissquare.qa.testcases;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.asserts.SoftAssert;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,6 +16,8 @@ public class RegisterTest extends BaseClass {
 	WebDriver driver;
 	RegisterPage registerPage;
 	DashboardPage dashboardPage;
+	SoftAssert softAssert;
+	//softAssert softAssert = new SoftAssert();
 
 	public RegisterTest() {
 		super();
@@ -22,7 +26,7 @@ public class RegisterTest extends BaseClass {
 	@BeforeMethod
 	public void setup() {
 		driver = initializeBrowserAndOpenApplicationURL(prop.getProperty("browserName"));
-		DashboardPage dashboardPage = new DashboardPage(driver);
+		dashboardPage = new DashboardPage(driver);
 		registerPage = dashboardPage.navigateToRegisterPage();
 	}
 
@@ -34,21 +38,64 @@ public class RegisterTest extends BaseClass {
 	@Test(priority = 1)
 	public void verifyRegisteringAnAccountWithMandatoryFields() {
 
-		// registerPage
-		// =registerPage.registerWithMandatoryFields(dataProp.getProperty("firstName"),dataProp.getProperty("lastName"),Utilities.generateEmailWithTimeStamp(),prop.getProperty("validPassword"));
 		registerPage = registerPage.registerWithMandatoryFields(dataProp.getProperty("firstName"),
 				dataProp.getProperty("lastName"), dataProp.getProperty("userName"), dataProp.getProperty("userEmail"),
 				dataProp.getProperty("userPhone"), dataProp.getProperty("userPassword"),
-				dataProp.getProperty("userRetypePasswords"));
-		// Assert.assertEquals(registerPage.retrieveAccountSuccessPageHeading().contains(dataProp.getProperty("accountSuccessfullyCreatedHeading")),"Congratulations!
-		// your account has been successfull");
-		System.out.println(dataProp.getProperty("lastName"));
+				dataProp.getProperty("userReTypePassword"), "Buyer");
+
 	}
 
 	public void verifyRegisteringAccountWithExistingEmailAddress() {
+
+		registerPage.registerWithMandatoryFields(dataProp.getProperty("firstName"), dataProp.getProperty("lastName"),
+				dataProp.getProperty("userName"), dataProp.getProperty("existingEmail"),
+				dataProp.getProperty("userPhone"), dataProp.getProperty("userPassword"),
+				dataProp.getProperty("userReTypePassword"), "Buyer");
+
+		Assert.assertTrue(driver.getPageSource().contains("already exists"),
+				"Existing email validation message not displayed");
 	}
 
 	public void verifyRegisteringAccountWithoutFillingAnyDetails() {
-	}
+		registerPage.registerWithMandatoryFields(
+                "", "", "", "", "", "", "", ""
+        );
+		
+        softAssert.assertEquals(
+                registerPage.retrieveFirstNameWarning(),
+                "The first name field is empty."
+        );
 
+        softAssert.assertEquals(
+                registerPage.retrieveLastNameWarning(),
+                "The last name field is empty."
+        );
+
+        softAssert.assertEquals(
+                registerPage.retrieveUserNameWarning(),
+                "The username field is empty."
+        );
+
+        softAssert.assertEquals(
+                registerPage.retrieveEmailWarning(),
+                "The email field is empty."
+        );
+
+        softAssert.assertEquals(
+                registerPage.retrievePhoneNumberWarning(),
+                "Please enter your number"
+        );
+
+        softAssert.assertEquals(
+                registerPage.retrievePasswordWarning(),
+                "One of the password field is empty!"
+        );
+
+        softAssert.assertEquals(
+                registerPage.retrieveTermsAndConditionsWarning(),
+                "You need to agree with terms & conditions."
+        );
+
+        softAssert.assertAll();
+    }
 }
